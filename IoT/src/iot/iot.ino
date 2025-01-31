aee31f62bd896ad346e9d73c07f3381feefca1f1
yonmaor1 <yonmaor@cmu.edu>
2025-01-31 10:55:46 -0500

    fixed src

--- IoT/src/iot/iot.ino
+++ IoT/src/iot/iot.ino
@@ -12,14 +12,14 @@
 
 //#define MQTT_TLS // uncomment this define to enable TLS transport
 //#define MQTT_TLS_VERIFY // uncomment this define to enable broker certificate verification
-const char* SSID = "yon"; // name of your wifi network
-const char* PASSWORD = "soPlayful"; // password of your wifi network
+const char* SSID = "yon";               // name of your wifi network
+const char* PASSWORD = "soPlayful";     // password of your wifi network
 const char* MQTT_SERVER = "pf-4pbo68mmklw1arbgld3b.cedalo.cloud";
 const uint16_t MQTT_SERVER_PORT = 1883; // unencrypted, unauthenticated server
-const char* MQTT_USER = ""; // name of client, set in platform.cedalo.cloud
-const char* MQTT_PASSWORD = ""; // password of client, set in platform.cedalo.cloud
-const char* MQTT_TOPIC_IN = ""; // what should we listen for
-const char* MQTT_TOPIC_OUT = ""; // what should we publish
+const char* MQTT_USER = "team1";        // name of client, set in platform.cedalo.cloud
+const char* MQTT_PASSWORD = "pass";     // password of client, set in platform.cedalo.cloud
+const char* MQTT_TOPIC_IN = "command";  // what should we listen for
+const char* MQTT_TOPIC_OUT = "data";    // what should we publish
 
 
 /* globals */
@@ -74,23 +74,48 @@ void setup_wifi() {
  */
 void callback(char* topic, byte* payload, unsigned int length) {
 
-}
+  // uncomment to see received messages in serial monitor
+  
+  Serial.print("Message arrived on topic: '");
+  Serial.print(topic);
+  Serial.print("' with payload: ");
+  for (unsigned int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  if ((char)payload[0] == 'T'){
+     digitalWrite(D0, HIGH);
+  } else {
+    digitalWrite(D0, LOW);
+  }
+} 
 
 /**
  * @brief connect to the mqtt broker
  */
 void connect() {
-  
+  while (!mqtt_client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    String mqtt_client_id = "1";
+    if (mqtt_client.connect(mqtt_client_id.c_str(), MQTT_USER, MQTT_PASSWORD)) {
+      Serial.println("connected");
+      mqtt_client.subscribe(MQTT_TOPIC_IN); // subscribe to the topic
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(mqtt_client.state());
+      Serial.println(" will try again in 5 seconds");
+      delay(5000);
+    }
+  }
 }
 
 //--------------------------------------
 void setup() {
-
-  Serial.begin(115200);
-  setup_wifi();          
-
-  // Serial.begin(115200);   // initialize serial communication
-  // mqtt_client.setCallback(callback);  // set callback function
+  Serial.begin(115200);   // initialize serial communication
+  setup_wifi();           // initialize wifi connection
+  mqtt_client.setServer(MQTT_SERVER, MQTT_SERVER_PORT); // initialize mqtt client
+  mqtt_client.setCallback(callback);  // set callback function
 
   // init pins
   pinMode(D0, OUTPUT);
@@ -100,6 +125,19 @@ void setup() {
 //--------------------------------------
 void loop() {
 
+  if (!mqtt_client.connected()) {
+    // if not connected, connect
+    connect();
+  }
+ 
+  mqtt_client.loop();
+  time_client.update();
+
+  // unconditionaly publish sensor data
+  int data = analogRead(A0);
+  String data_string = String(data);
+  Serial.println(data_string.c_str());
+  mqtt_client.publish(MQTT_TOPIC_OUT, data_string.c_str());
 
   // chill for a bit before next loop
   delay(10);
