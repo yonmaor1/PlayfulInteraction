90513121e4b256ad0c03b459830a662ed691b7e6
iwg-mk1 <86335822+iwg-mk1@users.noreply.github.com>
2025-08-28 18:52:57 -0400

    Update and rename musicbox.txt to musicbox.ino

---
+++ physical-computing/musicbox.ino
@@ -0,0 +1,21 @@
+int map(int val, int ilo, int ihi, int olo, int ohi) {
+  return olo + (ohi - olo) * ((val - ilo) / (ihi - ilo));
+}
+
+void setup() {
+  pinMode(A1, INPUT);
+  pinMode(A0, INPUT);
+}
+
+int volume;
+void loop() {
+
+  volume = analogRead(A1);
+
+  Serial.println(volume);
+
+  tone(A0, volume, 10);
+
+  // noTone(3);
+
+}
